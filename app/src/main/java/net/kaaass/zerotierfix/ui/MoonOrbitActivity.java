b4c2c3ae8020c79254f4ec5ff44c41e892f7145e
KAAAsS <admin@kaaass.net>
2021-04-07 20:59:05 +0800

    Add return button for sub page

--- app/src/main/java/net/kaaass/zerotierfix/ui/MoonOrbitActivity.java
+++ app/src/main/java/net/kaaass/zerotierfix/ui/MoonOrbitActivity.java
@@ -1,5 +1,9 @@
 package net.kaaass.zerotierfix.ui;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.ActionBar;
 import androidx.fragment.app.Fragment;
 
 /**
@@ -9,5 +13,27 @@ public class MoonOrbitActivity extends SingleFragmentActivity {
     @Override
     public Fragment createFragment() {
         return new MoonOrbitFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        // 添加返回按钮
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        // 返回上一界面
+        if (item.getItemId() == android.R.id.home) {
+            this.finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/net/kaaass/zerotierfix/ui/PrefsActivity.java
+++ app/src/main/java/net/kaaass/zerotierfix/ui/PrefsActivity.java
@@ -1,10 +1,39 @@
 package net.kaaass.zerotierfix.ui;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.ActionBar;
 import androidx.fragment.app.Fragment;
 
+/**
+ * 设置 fragment 的容器 activity
+ */
 public class PrefsActivity extends SingleFragmentActivity {
-    @Override // com.zerotier.one.ui.SingleFragmentActivity
+    @Override
     public Fragment createFragment() {
         return new PrefsFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        // 添加返回按钮
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        // 返回上一界面
+        if (item.getItemId() == android.R.id.home) {
+            this.finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
